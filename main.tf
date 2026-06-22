34f394aa69f4ceacd5e5c57d0b5de9739506a7ae
OM0126 <omgithub01@gmail.com>
2026-06-22 17:03:37 +0530

    Initial Terraform multiple region setup

---
+++ main.tf
@@ -0,0 +1,21 @@
+provider "aws" {
+region = "us-east-1"
+}
+
+resource "aws_instance" "server1" {
+ami           = "ami-0c02fb55956c7d316"
+instance_type = "t2.micro"
+
+tags = {
+Name = "Server-1"
+}
+}
+
+resource "aws_instance" "server2" {
+ami           = "ami-0c02fb55956c7d316"
+instance_type = "t2.micro"
+
+tags = {
+Name = "Server-2"
+}
+}
